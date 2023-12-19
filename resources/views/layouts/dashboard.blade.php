60d1903d7bd6852c3033c86f61e9d0e5f6a9b631
Ahmadtito17 <ahmadtitohendrawan@gmail.com>
2023-12-19 22:15:45 +0700

    Update Dashboard

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.index')
 
 @section('content')
-    <!-- Content Header (Page header) -->
+<div class="content-wrapper">
     <div class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
                     <h1 class="m-0">Dashboard</h1>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
+                </div>
+                <div class="col-sm-6"></div>
+            </div>
+        </div>
     </div>
-    <!-- /.content-header -->
-
+    
     <!-- Main content -->
     <section class="content">
         <div class="container-fluid">
@@ -65,6 +65,7 @@
         </div><!--/. container-fluid -->
     </section>
     <!-- /.content -->
+</div>
 @endsection
 
 @push('custom_js')

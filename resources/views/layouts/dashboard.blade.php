9998f86cb55794628460f6fb9961433d6203614d
Ahmadtito17 <ahmadtitohendrawan@gmail.com>
2023-12-19 22:06:54 +0700

    Update Layouts

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,68 +1,81 @@
 @extends('layouts.index')
 
 @section('content')
-<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
     <div class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
                     <h1 class="m-0">Dashboard</h1>
-                </div>
-                <div class="col-sm-6">
-                </div>
-            </div>
-        </div>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
     </div>
+    <!-- /.content-header -->
 
-    <div class="content">
+    <!-- Main content -->
+    <section class="content">
         <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="card bg-gradient-info">
-                        <div class="card-header border-0">
-                            <div class="d-flex justify-content-between align-items-center">
-                                <h2 class="card-title">Selamat Datang di Sistem Pendukung Keputusan</h2>
-                                <i class="fas fa-lightbulb fa-3x"></i>
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            <p class="lead text-white">
-                                Sistem ini dapat membantu Anda dalam pengambilan keputusan menggunakan Metode MOORA.
-                            </p>
-                            <p class="text-white">Metode ini diperkenalkan oleh Brauers pada tahun 2004 sebagai “Multi-Objective Optimization”. 
-                                Metode yang digunakan untuk mengevaluasi beberapa alternatif dalam sistem multi-kriteria. 
-                                Menentukan pilihan terbaik yang dioptimalkan berdasarkan kriteria yang diinginkan</p>
-                        </div>
+            <!-- Info boxes -->
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Selamat Datang</h3>
+
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                            <i class="fas fa-minus"></i>
+                        </button>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-    <div class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="card bg-gradient-success">
-                        <div class="card-header border-0">
-                            <div class="d-flex justify-content-between align-items-center">
-                                <h2 class="card-title text-white">Cara Penggunaan dalam pengambilan keputusan menggunakan Metode MOORA</h2>
-                                <i class="fas fa-info-circle fa-3x text-white"></i>
+                <div class="card-body bg-secondary">
+                    <div class="row">
+                        <a href="https://id.wikipedia.org/wiki/Sistem_pendukung_keputusan" class="col-12 col-sm-6 col-md-3">
+                            <div class="info-box mb-3">
+                                <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Tentang Moora</span>
+                                </div>
+                                <!-- /.info-box-content -->
                             </div>
-                        </div>
-                        <div class="card-body">
-                            <ol class="text-white">
-                                <li>Masuk ke menu "Kriteria & Bobot" untuk menambahkan kriteria beserta bobotnya.</li>
-                                <li>Gunakan menu "Alternatif & Skor" untuk menambahkan alternatif dan nilai skornya.</li>
-                                <li>Masuk ke menu "Matriks Keputusan" untuk melihat representasi matriks dari nilai skor alternatif pada setiap kriteria.</li>
-                                <li>Masuk ke menu "Perhitungann MOORA" untuk melakukan proses perhitungan normalisasi matriks keputusan, normalisasi terbobot, dan optimasi nilai Yi.</li>
-                                <li>Cek menu "Ranking" untuk melihat hasilnya.</li>
-                            </ol>
-                        </div>
+                            <!-- /.info-box -->
+                        </a>
+                        <!-- /.col -->
+
+                        <!-- fix for small devices only -->
+                        <div class="clearfix hidden-md-up"></div>
+
+                        <a href="https://e-journals.unmul.ac.id/index.php/JIM/article/view/5993"
+                            class="col-12 col-sm-6 col-md-3">
+                            <div class="info-box mb-3">
+                                <span class="info-box-icon bg-success elevation-1"><i class="fas fa-book"></i></span>
+
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Metode Moora</span>
+                                </div>
+                                <!-- /.info-box-content -->
+                            </div>
+                            <!-- /.info-box -->
+                        </a>
+                        <!-- /.col -->
                     </div>
                 </div>
+                <!-- /.card-body -->
             </div>
-        </div>
-    </div>
-    
-</div>
+            <!-- /.row -->
+        </div><!--/. container-fluid -->
+    </section>
+    <!-- /.content -->
 @endsection
+
+@push('custom_js')
+    @if ($message = Session::get('success'))
+        <script>
+            toastr.success('{{ $message }}')
+        </script>
+    @endif
+    @if ($message = Session::get('error'))
+        <script>
+            toastr.error('{{ $message }}')
+        </script>
+    @endif
+@endpush
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -8,59 +8,91 @@
                 <a class="d-block">SPK-MOORA</a>
             </div>
         </div>
-        <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <li class="nav-item">
-                    <a href="{{ url('dashboard') }}" class="nav-link">
-                        <i class="nav-icon fas fa-home"></i>
-                        <p>
-                            Home
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ url('kriteriabobot') }}" class="nav-link">
-                        <i class="nav-icon fas fa-balance-scale"></i>
-                        <p>
-                            Kriteria & Bobot
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ url('alternatif') }}" class="nav-link">
-                        <i class="nav-icon fas fa-list-alt"></i>
-                        <p>
-                            Alternatif & Skor
-                        </p>
-                    </a>
-                </li>
-                
-                <li class="nav-header">Hasil</li>
-                <li class="nav-item">
-                    <a href="{{ url('decisionmatrix') }}" class="nav-link">
-                        <i class="nav-icon fas fa-table"></i>
-                        <p>
-                            Matriks Keputusan
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ url('normalization') }}" class="nav-link">
-                        <i class="nav-icon far fa-chart-bar"></i>
-                        <p>
-                            Perhitungan MOORA
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ url('ranking') }}" class="nav-link">
-                        <i class="nav-icon fas fa-chart-line"></i>
-                        <p>
-                            Ranking
-                        </p>
-                    </a>
-                </li>
-            </ul>
-        </nav>
+    
+    <!-- Sidebar -->
+    <div class="sidebar">
+
+    <!-- Sidebar Menu -->
+    <nav class="h-100 d-flex flex-column justify-content-between align-items-center">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+            data-accordion="false">
+            <li class="nav-item">
+                <a href="{{ url('dashboard') }}" class="nav-link">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>
+                        Dashboard
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item menu-is-opening menu-open">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-database"></i>
+                    <p>
+                        Olah Data
+                        <i class="right fas fa-angle-left"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <ul style="list-style: none;">
+                        <li class="nav-item">
+                            <a href="{{ url('kriteriabobot') }}" class="nav-link">
+                                <i class="nav-icon fas fa-balance-scale-right"></i>
+                                <p>
+                                    Kriteria & Bobot
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ url('alternatif') }}" class="nav-link">
+                                <i class="nav-icon fas fa-list-ol"></i>
+                                <p>
+                                    Alternatif & Value
+                                </p>
+                            </a>
+                        </li>
+                    </ul>
+                </ul>
+            </li>
+            <li class="nav-item menu-is-opening menu-open">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-calculator"></i>
+                    <p>
+                        Hasil
+                        <i class="right fas fa-angle-left"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <ul style="list-style: none;">
+                        <li class="nav-item">
+                            <a href="{{ url('decisionmatrix') }}" class="nav-link">
+                                <i class="nav-icon fas fa-table"></i>
+                                <p>
+                                    Matriks Keputusan
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ url('normalization') }}" class="nav-link">
+                                <i class="nav-icon fas fa-chart-area"></i>
+                                <p>
+                                    Perhitungan MOORA
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ url('ranking') }}" class="nav-link">
+                                <i class="nav-icon fas fa-sort-amount-down"></i>
+                                <p>
+                                    Ranking
+                                </p>
+                            </a>
+                        </li>
+                    </ul>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
     </div>
-</aside>
+    <!-- /.sidebar -->
+    </aside>
